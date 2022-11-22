e1e43888c3ed4973a079d7ca5e91c4c8178437e6
yk271 <29210205+yk271@users.noreply.github.com>
2022-11-23 04:36:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,19 +15,20 @@
 
 # 删除密码
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' ./package/lean/default-settings/files/zzz-default-settings
-
-# 添加软件源码
+# 引入 AdGuardHome Luci，暂不编译核心（可能导致“启动项”中出现两个 AdGuardHome）
 git clone https://github.com/yk271/luci-app-adguardhome.git ./package/luci-app-adguardhome
-
+# Lede 自带 DDNS 插件似乎有 BUG，可能导致 Luci 卡顿，暂时使用独立插件
 git clone https://github.com/yk271/openwrt-ddns-app.git ./package/openwrt-ddns-app
-
+# 引入 Passwall 软件包和 Luci
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
+# 修改 Passwall 直连和代理列表
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
-
+# 引入老竭力的 Argon 主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 ./package/luci-theme-argon
 
+# 【临时】回退 Passwall Luci 版本，排查 DNS 异常的问题
 cd ./package/passwall_luci
 git checkout 0b925ce8920c0c127e26ca9d5e4458ab2eb401db
 cd ../../
