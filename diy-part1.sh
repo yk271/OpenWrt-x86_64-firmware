20e8fc04335da1e044417bf4d522a22f4c5d9d2e
yk271 <29210205+yk271@users.noreply.github.com>
2022-12-10 23:15:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 # 删除密码
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' ./package/lean/default-settings/files/zzz-default-settings
 
-git clone https://github.com/xiaorouji/luci-app-adguardhome.git ./package/luci-app-adguardhome
+git clone https://github.com/yk271/luci-app-adguardhome.git ./package/luci-app-adguardhome
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/yk271/openwrt-passwall.git -b luci ./package/passwall_luci
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
