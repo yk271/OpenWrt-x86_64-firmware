964a6cd8d9f63c479fb1da64d9428f2a0d06c21b
yk271 <29210205+yk271@users.noreply.github.com>
2023-01-05 00:01:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,3 +11,7 @@
 #
 
 git clone https://github.com/yk271/luci-app-adguardhome.git ./package/luci-app-adguardhome
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
+wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
