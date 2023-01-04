e565d1306ce7ba54a366d653317bdf827070751d
yk271 <29210205+yk271@users.noreply.github.com>
2023-01-04 23:54:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,4 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 修改内核版本
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' ./target/linux/x86/Makefile
-# 删除密码
-# sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' ./package/lean/default-settings/files/zzz-default-settings
-
 git clone https://github.com/yk271/luci-app-adguardhome.git ./package/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
-wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
-wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
