0b73e443b47c312f88199168c10f28eebd8d93e9
yk271 <29210205+yk271@users.noreply.github.com>
2023-03-02 02:50:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,11 @@
 # git checkout 727d803cb09ad4bd2bb1b178f9fd44e9bf1f2595 ./package/network/services/dnsmasq
 
 # git clone https://github.com/yk271/luci-app-adguardhome-1.git ./package/luci-app-adguardhome
+
 git clone https://github.com/yk271/openwrt-passwall.git -b packages ./package/passwall_package
 git clone https://github.com/yk271/openwrt-passwall.git -b luci ./package/passwall_luci
+# rm -rf ./package/passwall_luci/luci-app-passwall/root/etc/hotplug.d
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+
 git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon-mod
