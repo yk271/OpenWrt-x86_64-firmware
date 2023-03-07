01108e3181e911e7bf193f48fb227f636c919b52
yk271 <29210205+yk271@users.noreply.github.com>
2023-03-08 02:07:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,11 @@
 
 # git checkout c2564b980dc41f251a7b010fe665c9176abefc4b ./package/default-settings/files/zzz-default-settings
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
-# rm -rf ./package/passwall_luci/luci-app-passwall/root/etc/hotplug.d
-wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
-wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+git clone https://github.com/yk271/openwrt-passwall.git -b packages ./ext_package/passwall_package
+git clone https://github.com/yk271/openwrt-passwall.git -b luci ./ext_package/passwall_luci
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/luci-theme-argon-mod
+wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./ext_package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
+wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./ext_package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git ./ext_package/luci-theme-argon-mod
+git clone https://github.com/yk271/luci-app-adguardhome.git ./ext_package/luci-app-adguardhome
