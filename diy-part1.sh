45c06524282fe0ab6e30786b7ea1ba4583490d82
yk271 <29210205+yk271@users.noreply.github.com>
2023-03-07 22:27:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-git checkout c2564b980dc41f251a7b010fe665c9176abefc4b ./package/default-settings/files/zzz-default-settings
+# git checkout c2564b980dc41f251a7b010fe665c9176abefc4b ./package/default-settings/files/zzz-default-settings
 
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
