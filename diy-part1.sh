1ecab86ae2460b8c4fdcf95bc0cf523fafa487ac
Yang Ting <29210205+yk271@users.noreply.github.com>
2023-06-17 14:59:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,10 @@
 # 回退 dnsmasq 版本
 #git checkout 81a3a0ff39290e874d9ef9c9db52d2dfae9f40ca ./package/network/services/dnsmasq
 
-git clone https://github.com/kongfl888/luci-app-adguardhome.git ./package/luci-app-adguardhome
-git clone https://github.com/yk271/openwrt-passwall.git -b packages ./package/passwall_package
-git clone https://github.com/yk271/openwrt-passwall.git -b luci ./package/passwall_luci
+git clone https://github.com/yk271/luci-app-adguardhome.git ./package/luci-app-adguardhome
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./package/passwall_package
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 ./package/luci-theme-argon
