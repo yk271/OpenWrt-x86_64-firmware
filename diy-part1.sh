8862e48d28618f48679c97cfb68c3d673fa68a55
Yang Ting <29210205+yk271@users.noreply.github.com>
2023-08-01 23:43:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages ./packag
 git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci ./package/passwall_luci
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
+wget https://raw.githubusercontent.com/yk271/proxy-rule/main/0_default_config -O ./package/passwall_luci/luci-app-passwall/root/usr/share/passwall/0_default_config
 rm -rf ./package/passwall_luci/luci-app-passwall/root/etc/hotplug.d
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 ./package/luci-theme-argon-18.06
