e5d7cfc0e10625262462e69d2ef5602c823b359e
yk271 <29210205+yk271@users.noreply.github.com>
2023-08-09 05:37:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,3 @@ rm -rf ./feeds/luci/applications/luci-app-passwall/root/etc/hotplug.d
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/0_default_config -O ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/0_default_config
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/direct_host.txt -O ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/direct_host
 wget https://raw.githubusercontent.com/yk271/proxy-rule/main/proxy_host.txt -O ./feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/rules/proxy_host
-
-git clone https://github.com/yk271/luci-app-adguardhome.git ./feeds/luci/applications/luci-app-adguardhome
